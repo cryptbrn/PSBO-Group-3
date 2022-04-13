fb4c3f26d363cf8198fa4289b7e59ecd64abf840
cryptbrn <cryptbrnreyhan@apps.ipb.ac.id>
2022-04-13 22:59:51 +0700

    init

---
+++ src/main/java/com/example/psbogroup3/Psbogroup3Application.java
@@ -0,0 +1,13 @@
+package com.example.psbogroup3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Psbogroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Psbogroup3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/psbogroup3/Psbogroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psbogroup3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Psbogroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
