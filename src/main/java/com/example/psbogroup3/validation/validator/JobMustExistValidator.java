8529eaa207aec54d60112c7c5416b109053edb36
Abdul Hakim <akimpiy@gmail.com>
2022-06-06 08:20:28 +0700

    Add job id, add controller to create, find by id, delete by id

---
+++ src/main/java/com/example/psbogroup3/validation/validator/JobMustExistValidator.java
@@ -0,0 +1,25 @@
+package com.example.psbogroup3.validation.validator;
+
+import com.example.psbogroup3.repository.JobRepository;
+import com.example.psbogroup3.validation.JobMustExist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class JobMustExistValidator implements
+        ConstraintValidator<JobMustExist, String> {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (!StringUtils.hasText(value)) {
+            return true;
+        }
+
+        return jobRepository.existsById(value);
+    }
+}
--- src/main/java/com/example/psbogroup3/web/model/request/UpdateJobRequest.java
+++ src/main/java/com/example/psbogroup3/web/model/request/UpdateJobRequest.java
@@ -1,21 +1,15 @@
-package com.example.psbogroup3.entity;
+package com.example.psbogroup3.web.model.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-@Document("job")
-
-public class Job {
-    @Id
-    private String id;
+public class UpdateJobRequest {
     private String institution;
     private String position;
     private double salary;
