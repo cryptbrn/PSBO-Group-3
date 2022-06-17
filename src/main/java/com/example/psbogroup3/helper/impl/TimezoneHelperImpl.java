cf99b6debcc52a86688e4af6c47a23ef512e514f
cryptbrn <cryptbrn@gmail.com>
2022-06-17 18:10:32 +0700

    add checking in timezone helper

--- src/main/java/com/example/psbogroup3/helper/impl/TimezoneHelperImpl.java
+++ src/main/java/com/example/psbogroup3/helper/impl/TimezoneHelperImpl.java
@@ -16,9 +16,12 @@ public class TimezoneHelperImpl implements TimezoneHelper {
 
   @Override
   public String generateOffset(String location) {
-    TimeZone tz = TimeZone.getTimeZone(location);
-    Calendar cal = GregorianCalendar.getInstance(tz);
-    int offsetInMillis = tz.getOffset(cal.getTimeInMillis());
+    TimeZone timeZone = TimeZone.getTimeZone(location);
+    if(!timeZone.getID().equals(location)){
+      return "TIME-ZONE-NOT-VALID";
+    }
+    Calendar calendar = GregorianCalendar.getInstance(timeZone);
+    int offsetInMillis = timeZone.getOffset(calendar.getTimeInMillis());
     String offset = String.format("%02d:%02d", Math.abs(offsetInMillis / 3600000),
         Math.abs((offsetInMillis / 60000) % 60));
     return "GMT" + (offsetInMillis >= 0 ? "+" : "-") + offset;
